07dd69f8eadc43a132483e8d107c9b74275f04a0
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-10-13 18:18:47 -0300

    feat: adiciona header bar para templates

--- src/pages/templates/index.tsx
+++ src/pages/templates/index.tsx
@@ -2,9 +2,12 @@ import { setTemplates } from "@/store/templates";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { TemplateCards } from "./_components/templates-cards";
+import { ArrowLeft, Plus } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Template() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:3000/templates").then(async (data) => {
@@ -14,5 +17,20 @@ export default function Template() {
     });
   }, []);
 
-  return <TemplateCards />;
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-between items-center bg-[#202c33] w-full h-14 px-4">
+        <ArrowLeft
+          className="cursor-pointer text-white"
+          onClick={() => {
+            navigate(`/`);
+          }}
+        />
+
+        <Plus className="cursor-pointer text-white" />
+      </div>
+
+      <TemplateCards />
+    </div>
+  );
 }
