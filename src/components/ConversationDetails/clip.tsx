fb7cfa8ae6b808608003f1cdfdd24287efd25146
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-10-18 11:00:53 -0300

    feat: adiciona envio de templates em conversas

--- src/components/ConversationDetails/clip.tsx
+++ src/components/ConversationDetails/clip.tsx
@@ -1,8 +1,53 @@
 import { BookTemplate, Paperclip } from "lucide-react";
 import { Popover, PopoverTrigger, PopoverContent } from "../ui/popover";
 import { Icon } from "../ui/icon";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+} from "../ui/alert-dialog";
+import { TemplateCards } from "@/pages/templates/_components/templates-cards";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setTemplates } from "@/store/templates";
+import { Template } from "@/models/template";
+import { useParams } from "react-router-dom";
 
 export const Clip = () => {
+  const dispatch = useDispatch();
+
+  const { to } = useParams();
+
+  useEffect(() => {
+    fetch("http://localhost:3000/templates").then(async (data) => {
+      const templates = await data.json();
+
+      dispatch(setTemplates(templates));
+    });
+  }, []);
+
+  const sendTemplate = ({ name, language }: Template) => {
+    const dto = {
+      to,
+      template: {
+        name,
+        language: {
+          code: language,
+        },
+      },
+    };
+
+    const headers = new Headers();
+
+    headers.append("Content-Type", "application/json");
+
+    fetch("http://localhost:3000/messages/template", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(dto),
+    });
+  };
+
   return (
     <div>
       <Popover>
@@ -11,9 +56,22 @@ export const Clip = () => {
         </PopoverTrigger>
 
         <PopoverContent>
-          <Icon onClick={() => {}} className="bg-green-500">
-            <BookTemplate aria-label="Templates" />
-          </Icon>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Icon className="bg-green-500">
+                <BookTemplate aria-label="Templates" />
+              </Icon>
+            </AlertDialogTrigger>
+
+            <AlertDialogContent>
+              <div className="h-[400px] overflow-y-scroll">
+                <TemplateCards
+                  onlyVerify
+                  onClick={(template) => sendTemplate(template)}
+                />
+              </div>
+            </AlertDialogContent>
+          </AlertDialog>
         </PopoverContent>
       </Popover>
     </div>
--- src/pages/templates/_components/templates-cards.tsx
+++ src/pages/templates/_components/templates-cards.tsx
@@ -1,17 +1,45 @@
 import { TemplateCard } from "@/components/TemplateCard";
+import { Template } from "@/models/template";
 import { selectTemplates } from "@/store/templates";
 import { useSelector } from "react-redux";
 
-export const TemplateCards = () => {
+export type TemplateCardProps = {
+  onlyVerify?: boolean;
+  onClick?: (template: Template) => void;
+};
+
+export const TemplateCards = ({ onClick, onlyVerify }: TemplateCardProps) => {
   const templates = useSelector(selectTemplates);
+
+  if (onlyVerify) {
+    return (
+      <div className="flex flex-col gap-3">
+        {templates.map(
+          (template) =>
+            template.status === "APPROVED" && (
+              <div
+                key={`template-card${template.id}`}
+                onClick={() => onClick && onClick(template)}
+                className={onClick && `cursor-pointer`}
+              >
+                <TemplateCard {...{ template, className: "text-white" }} />
+              </div>
+            )
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-3">
       {templates.map((template) => (
-        <TemplateCard
+        <div
           key={`template-card${template.id}`}
-          {...{ template, className: "text-white" }}
-        />
+          onClick={() => onClick && onClick(template)}
+          className={onClick && `cursor-pointer`}
+        >
+          <TemplateCard {...{ template, className: "text-white" }} />
+        </div>
       ))}
     </div>
   );
