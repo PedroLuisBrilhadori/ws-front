d13eda088ca9ad513f14a589d41f7deca6d55d88
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-11-08 19:59:50 +0000

    Add telephone number validation and formatting to NewConversation component

--- src/components/ConversationList/new-conversation.tsx
+++ src/components/ConversationList/new-conversation.tsx
@@ -6,13 +6,36 @@ import {
 } from "../ui/alert-dialog";
 import { Icon } from "../ui/icon";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  removeTelephoneMask,
+  telephoneMask,
+  validNumber,
+} from "@/lib/telephone";
 
 export const NewConversation = () => {
   const [open, setOpen] = useState(false);
-  const [number, setNumber] = useState("");
+  const numberRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (event.type !== "submit") return;
+
+    const number = numberRef.current?.value;
+
+    if (!number) return;
+
+    console.log(validNumber(number));
+
+    if (!validNumber(number)) return;
+
+    const num = removeTelephoneMask(number);
+
+    navigate(`conversation/${num}`);
+  };
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
@@ -23,32 +46,39 @@ export const NewConversation = () => {
       </AlertDialogTrigger>
 
       <AlertDialogContent>
-        <div className="flex flex-col gap-3">
+        <form className="flex flex-col gap-3" onSubmit={onSubmit}>
           <h1>Nova conversa</h1>
-
           <Input
+            ref={numberRef}
             className=""
             placeholder="Numero..."
-            onChange={(e) => setNumber(e.target.value)}
+            maxLength={18}
+            onChange={(e) => {
+              if (!e.target.value) return;
+
+              const num = telephoneMask(e.target.value);
+
+              e.target.value = num;
+            }}
           ></Input>
 
           <div className="flex gap-3 self-end">
             <Icon
+              onClick={() => {
+                setOpen(false);
+              }}
               className="border-2 border-red-500"
-              onClick={() => setOpen(false)}
             >
               <X className=" text-red-500" />
             </Icon>
-            <Icon
-              className="bg-green-500"
-              onClick={() => {
-                number !== "" && navigate(`conversation/${number}`);
-              }}
-            >
-              <Check />
-            </Icon>
+
+            <button>
+              <Icon className="bg-green-500">
+                <Check />
+              </Icon>
+            </button>
           </div>
-        </div>
+        </form>
       </AlertDialogContent>
     </AlertDialog>
   );
---
+++ src/lib/telephone.ts
@@ -0,0 +1,25 @@
+export const validNumber = (number: string) => {
+  if (number.length != 18) return false;
+
+  return number !== "";
+};
+
+export const telephoneMask = (value: string) => {
+  const phone = value.replace(/\D/g, "");
+
+  if (phone.length === 12) {
+    return phone
+      .replace(/(\d{2})(\d)/, "+$1 ($2")
+      .replace(/(\d{2})(\d)/, "$1) $2")
+      .replace(/(\d{4})(\d{4})$/, "$1-$2");
+  } else {
+    return phone
+      .replace(/(\d{2})(\d)/, "+$1 ($2")
+      .replace(/(\d{2})(\d)/, "$1) $2")
+      .replace(/(\d{1})(\d{4})(\d{4})$/, "$1 $2-$3");
+  }
+};
+
+export const removeTelephoneMask = (value: string) => {
+  return value.replace(/\+|\-|\(|\)|\ /g, "");
+};
