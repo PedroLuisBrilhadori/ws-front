ab23f6cceb30b2066e0d7d6dd2f27b38b0abdfa2
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-12-13 04:24:37 -0300

    Refactor SideBar component and add search functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
 import { useDispatch } from "react-redux";
-import SideBar from "../components/SideBar";
 import { clearCurrentConversation } from "@/store/currentConversation";
+import { useState } from "react";
+import { SideBarSearch } from "@/components/SideBar/search";
+import ConversationList from "@/components/ConversationList";
 
 export default function Home() {
   const dispatch = useDispatch();
+  const [search, setSearch] = useState("");
 
   dispatch(clearCurrentConversation(false));
 
   return (
     <div className="flex justify-center">
-      <div className="flex w-full h-screen">
-        <SideBar />
+      <div
+        className="flex flex-col w-full h-full "
+        style={{ borderRight: "1px solid rgba(134,150,160,0.15)" }}
+      >
+        <SideBarSearch search={search} setSearch={setSearch} />
+
+        <div
+          className="flex flex-col w-full overflow-y-scroll"
+          id="conversation"
+        >
+          <ConversationList search={search} />
+        </div>
       </div>
     </div>
   );
